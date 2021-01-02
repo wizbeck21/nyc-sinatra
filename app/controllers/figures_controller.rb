5745de5a4559c641e4ed7be1186ae92e595fe4be
Wesley <wes.beck21@outlook.com>
2021-01-01 20:41:23 -0800

    commit tree 50c827e2de88f779d5435e5f7456311227a50580

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,54 @@
 class FiguresController < ApplicationController
   # add controller methods
+
+  get '/figures' do #index
+    @figures = Figure.all
+    @landmarks = Landmark.all
+    erb :"figures/index"
+  end
+
+    get '/figures/new' do #new
+      erb :"figures/new"
+    end
+
+    post '/figures' do #create
+      @figure = Figure.create(params["figure"])
+      unless params[:landmark][:name].empty?
+        @figure.landmarks << Landmark.create(params[:landmark])
+      end
+      unless params[:title][:name].empty?
+        @figure.titles << Title.create(params[:title])
+      end
+      @figure.save
+      redirect to "/figures/#{@figure.id}"
+    end
+
+    get '/figures/:id' do
+      @figure = Figure.find(params[:id])
+      erb :"figures/show"
+    end
+
+    get '/figures/:id/edit' do
+      @figure = Figure.find(params[:id])
+      erb :"figures/edit"
+    end
+
+    patch '/figures/:id' do
+      @figure = Figure.find_by_id(params[:id])
+      @figure.update(params[:figure])
+
+      unless params[:title][:name].empty?
+        @figure.titles << Title.create(params[:title])
+      end
+      unless params[:landmark][:name].empty?
+        @figure.landmarks << Landmark.create(params[:name])
+      end
+
+      @figure.save
+      redirect to "/figures/#{@figure.id}"
+    end
+
+
+      
+
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,37 @@
 class LandmarksController < ApplicationController
   # add controller methods
+
+  get '/landmarks' do #index
+    @landmarks = Landmark.all
+    @figures = Figure.all
+    erb :"landmarks/index"
+  end
+  
+  get '/landmarks/new' do #new form
+    erb :"/landmarks/new"
+  end
+
+  post '/landmarks' do #create
+    Landmark.create(name: params["landmark"]["name"], year_completed: params["landmark"]["year_completed"])
+    redirect to "/landmarks/:id"
+  end
+
+  get '/landmarks/:id' do #show page
+    @landmark = Landmark.find(params[:id])
+    erb :"landmarks/show"
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :"landmarks/edit"
+  end
+
+  post '/landmarks/:id' do #edit landmark
+    @landmark = Landmark.find(params[:id])
+    @landmark.name = params["landmark"]["name"]
+    @landmark.year_completed = params["landmark"]["year_completed"]
+    @landmark.save
+    redirect to "/landmarks/#{@landmark.id}"
+  end
+
 end
